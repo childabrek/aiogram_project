5654d2ebdb3056671c4af0ef158a030a24b89721
Adel-15-Ivv <adelinaivanova30@gmail.com>
2024-11-29 13:56:57 +0500

    dfhdfhdf

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import json
 import logging
 import asyncio
-from datetime import datetime
+from sched import scheduler
+
 from aiogram import Dispatcher, Bot, types
 from aiogram.filters import Command, Filter
 from aiogram.types import FSInputFile
 
 logging.basicConfig(level=logging.INFO)
-TOKEN = '7605960349:AAECPUTKkAm_8RR1wvWWMQKYYv8OS8AgQ2Y'
+TOKEN = '8122833408:AAFdg78LuB8AJFWUFaeU4pB8bMJB_uBM3Lo'
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
@@ -23,39 +23,12 @@ async def echo(message: types.Message):
     a = FSInputFile("1.jpg")
     await message.answer_photo(a)
 
+# Илья Маслов
+async def wake_up_members():
+    await bot.send_message(chat_id='-1002312275639', text="Время вставать!")
 
-with open('words.json', 'r', encoding='utf-8') as f:
-    bad_words_data = json.load(f)
-    bad_words = [item['word'] for item in bad_words_data]
 
-user_last_deleted_time = {}
-
-@dp.message(Command('delete'))
-async def delete_message(msg: types.Message):
-    if msg.reply_to_message:
-        message_id_to_delete = msg.reply_to_message.message_id
-        chat_id = msg.chat.id
-
-        await bot.delete_message(chat_id, message_id_to_delete)
-        await msg.answer("Сообщение удалено.")
-
-        user_last_deleted_time[msg.from_user.id] = datetime.now()
-    else:
-        await msg.answer("Пожалуйста, ответьте на сообщение, которое хотите удалить.")
-
-@dp.message()
-async def auto_delete_message(msg: types.Message):
-    if any(bad_word in msg.text.lower() for bad_word in bad_words):
-        await bot.delete_message(msg.chat.id, msg.message_id)
-        logging.info(f"Удалено сообщение: {msg.text}")
-
-        user_last_deleted_time[msg.from_user.id] = datetime.now()
-
-    if msg.from_user.id in user_last_deleted_time:
-        last_deleted_time = user_last_deleted_time[msg.from_user.id]
-        if datetime.now() - last_deleted_time <= timedelta(minutes=30):
-            await bot.delete_message(msg.chat.id, msg.message_id)
-
+scheduler.add_job(wake_up_members, 'cron', hour=13, minute=39)
 
 
 async def start_dp():
