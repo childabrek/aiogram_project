62e538ac4fc2533935b68da47c96c5425ce82c2c
Belgia <sdfghsdfbsfbg@gmail.com>
2024-11-29 16:38:11 +0500

    jm

--- main.py
+++ main.py
@@ -1,64 +1,13 @@
-import logging
-import asyncio
-from sched import scheduler
-
-from aiogram import Dispatcher, Bot, types
-from aiogram.filters import Filter
-from aiogram import F
-from aiogram.types import Message, FSInputFile, inline_query_results_button, ReplyKeyboardMarkup
-from aiogram.filters import Command
-from aiogram.enums import ParseMode
-import yandex_weather_api
-
-logging.basicConfig(level=logging.INFO)
-TOKEN = '8122833408:AAFdg78LuB8AJFWUFaeU4pB8bMJB_uBM3Lo'
-
-bot = Bot(token=TOKEN)
-dp = Dispatcher()
-
-# Привет это Никита
-
-# keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, keyboard=[])
-#
-# keyboard.
-
-class MyFilter(Filter):
-    def __init__(self, my_text: str) -> None:
-        self.my_text = my_text
-
-    async def __call__(self, message: Message) -> bool:
-        return message.text == self.my_text
+import requests
+from bs4 import BeautifulSoup
 
 
-@dp.message(Command("start"))
-async def start(message: types.Message):
-    button = inline_query_results_button.InlineQueryResultsButton(text='test')
-    await message.reply('Hello world!' + message.from_user.username, parse_mode='pre-formatted fixed-width code block')
+
+url = 'https://journal.top-academy.ru/en/main/schedule/page/index'
+response = requests.get(url)
+response.raise_for_status()
 
 
-@dp.message(F.text, Command("test"))
-async def any_message(message: Message):
-    a = FSInputFile.read
-    await message.answer(
-        "Hello, ```world```\!",
-        parse_mode=ParseMode.MARKDOWN_V2
-    )
-
-# Илья Маслов
-async def wake_up_members():
-    await bot.send_message(chat_id='-1002312275639', text="Время вставать!")
-
-
-@dp.message(MyFilter("Хакнуть Илью"))
-async def hack(message: Message):
-    await message.answer(message.text.split()[1])
-
-scheduler.add_job(wake_up_members, 'cron', hour=13, minute=39)
-
-
-async def start_dp():
-    await dp.start_polling(bot)
-
-
-if __name__ == '__main__':
-    asyncio.run(start_dp())
+soup = BeautifulSoup(response.text, 'html.parser')
+TEXT = soup.findAll('div', class_='time-content__item')
+print(TEXT)
