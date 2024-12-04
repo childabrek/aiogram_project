d9677141c74918b480a60811caa2fbac8bda1a5f
adelinakim7 <adelin4ik91@gmail.com>
2024-12-04 12:47:14 +0500

    Бdtdjd

--- main.py
+++ main.py
@@ -1,39 +1,80 @@
-import logging
 import asyncio
-from sched import scheduler
+import Password
+import logging
+from datetime import datetime
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-from aiogram import Dispatcher, Bot, types
-from aiogram.filters import Command, Filter
-from aiogram.types import FSInputFile
 
+
+# Настройка логгера
 logging.basicConfig(level=logging.INFO)
-TOKEN = '8122833408:AAFdg78LuB8AJFWUFaeU4pB8bMJB_uBM3Lo'
+logger = logging.getLogger
 
-bot = Bot(token=TOKEN)
+CHAT_ID = '-1002312275639'
+
+
+bot = Bot(token=Password.TOKEN)
 dp = Dispatcher()
 
+# Дни рождения
+birthdays = {
+    'Никиту Владиславовича': '07.02',
+    'Регину Маратовну': '17.06',
+    'Виктрорию Александровну': '09.03',
+    'Владимира Владиславовича': '17.12',
+    'Марата': '02.11',
+    'Артура': '17.12',
+    'Егора': '25.06',
+    'Розу': '03.07',
+    'Владислава': '30.05',
+    'Данила': '24.08',
+    'Ралима': '06.09',
+    'Карпенко Артема': '03.04',
+    'Маслова Илью': '16.08',
+    'Ярослава': '22.01',
+    'Парфёнова Илью': '08.12',
+    'Кирилла': '13.02',
+    'Софью': '17.12',
+    'Фаезбека': '07.02',
+    'Арслана': '16.04',
+    'Ченцова Артема': '05.02',
+    'Наиля': '08.12',
+    'Хаяла': '18.09',
+}
 
-@dp.message(Command("start"))
+
+# Команда /start
+@dp.message(Command('HappyBirthday'))
 async def start(message: types.Message):
-    await message.reply('Hello world!' + message.from_user.username, parse_mode='pre-formatted fixed-width code block')
+    await message.reply('Hello Friend!' + message.from_user.username)
 
 
-@dp.message()
-async def echo(message: types.Message):
-    a = FSInputFile("1.jpg")
-    await message.answer_photo(a)
-
-# Илья Маслов
-async def wake_up_members():
-    await bot.send_message(chat_id='-1002312275639', text="Время вставать!")
+# Планировщик задач
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(check_and_send_birthday, 'cron', hour=10, minute=00)
+    scheduler.start()
 
 
-scheduler.add_job(wake_up_members, 'cron', hour=13, minute=39)
+# Проверка и отправка поздравлений
+async def check_and_send_birthday():
+    today = datetime.now().strftime('%d.%m')
+    for name, date in birthdays.items():
+        if date == today:
+            message = f"🎉 Поздравляем {name} с Днем Рождения! 🥳🎂"
+            await bot.send_message(chat_id=CHAT_ID, text=message)
+            logger(f"Поздравление отправлено: {name}")
 
 
+# Запуск диспетчера
 async def start_dp():
     await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
     asyncio.run(start_dp())
+
+
+
+#суприм
