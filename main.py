4f60a10fbe8381ff540f913c6170feebc7e4c74e
Adel-15-Ivv <adelinaivanova30@gmail.com>
2024-12-04 10:11:06 +0500

    я сделал будильник с возможностью изменения времени в чате.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+
+from aiogram.types import FSInputFile
 import logging
 import asyncio
-from aiogram import Dispatcher, Bot, types
-from aiogram.filters import Command, Filter
-from aiogram.types import FSInputFile
-
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 logging.basicConfig(level=logging.INFO)
 TOKEN = '8122833408:AAFdg78LuB8AJFWUFaeU4pB8bMJB_uBM3Lo'
 
@@ -22,6 +23,45 @@ async def echo(message: types.Message):
     await message.answer_photo(a)
 
 
+# илья маслов
+scheduler = AsyncIOScheduler()
+
+scheduled_hour = 7
+scheduled_minute = 5
+
+async def wake_up_members():
+    await bot.send_message(chat_id='-1002312275639', text="Время вставать!")
+
+
+scheduler.add_job(wake_up_members, 'cron', hour=scheduled_hour, minute=scheduled_minute, id='wake_up_job')
+
+@dp.message(Command('set_time'))
+async def set_time(message: types.Message):
+    global scheduled_hour, scheduled_minute
+
+    time_data = message.text.split(' ')[1:]
+    if len(time_data) != 2:
+        await message.reply("Укажите время в формате: /set_time ЧЧ ММ")
+        return
+
+    try:
+        hour, minute = int(time_data[0]), int(time_data[1])
+        if 0 <= hour < 24 and 0 <= minute < 60:
+            scheduled_hour = hour
+            scheduled_minute = minute
+
+            scheduler.remove_job('wake_up_job')
+            scheduler.add_job(wake_up_members, 'cron', hour=scheduled_hour, minute=scheduled_minute, id='wake_up_job')
+
+            await message.reply(f"Время было обновлено на: {scheduled_hour}:{scheduled_minute:02d}")
+        else:
+            await message.reply("Некорректное время. Часы должны быть от 0 до 23, минуты от 0 до 59.")
+    except ValueError:
+        await message.reply("Пожалуйста, используйте целые числа для часового и минутного значений.")
+
+@dp.message(Command('chatid'))
+async def chat_id(message: types.Message):
+    await message.reply(f'ID чата: {message.chat.id}')
 
 async def start_dp():
     await dp.start_polling(bot)
