3522c899916b5f1ca707a8a857ccc3634ca7f0aa
Arslan1982 <arslan1982monica@gmail.com>
2024-12-07 11:07:34 +0300

    main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging
 import asyncio
 from aiogram import Dispatcher, Bot, types
 from aiogram.filters import Command
+import json
 
 logging.basicConfig(level=logging.INFO)
 
@@ -10,12 +11,42 @@ import password
 bot = Bot(token=password.Token)
 dp = Dispatcher()
 
+users_url = "https://msapi.top-academy.ru/api/v2/auth/login"
+payload = json.dumps({
+  "application_key": "6a56a5df2667e65aab73ce76d1dd737f7d1faef9c52e8b8c55ac75f565d8e8a6",
+  "id_city": None,
+  "password": "OrdA2020",
+  "username": "Husnu_ii62"
+})
+users_headers = {
+  'accept': 'application/json, text/plain, */*',
+  'accept-language': 'ru_RU, ru',
+  'authorization': 'Bearer null',
+  'content-type': 'application/json',
+  'origin': 'https://journal.top-academy.ru',
+  'priority': 'u=1, i',
+  'referer': 'https://journal.top-academy.ru/',
+  'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+  'sec-ch-ua-mobile': '?0',
+  'sec-ch-ua-platform': '"Windows"',
+  'sec-fetch-dest': 'empty',
+  'sec-fetch-mode': 'cors',
+  'sec-fetch-site': 'same-site',
+  'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+  'Cookie': '_csrf=PHrhu1IbcCqAafKD6FydbdEiDt93l5vT'
+}
+
+user_response = requests.post(users_url, headers=users_headers, data=payload)
+user_data = user_response.json()
+user_token = user_data.get('access_token', '')
+
+
 async def get_lessons():
     url = "https://msapi.top-academy.ru/api/v2/schedule/operations/get-by-date"
     headers = {
         'accept': 'application/json, text/plain, */*',
         'accept-language': 'ru_RU, ru',
-        'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvbXNhcGkuaXRzdGVwLm9yZyIsImlhdCI6MTczMzU0NDI5MywiYXVkIjoxLCJleHAiOjE3MzM1NjU4OTMsImFwaUFwcGxpY2F0aW9uSWQiOjEsImFwaVVzZXJUeXBlSWQiOjEsInVzZXJJZCI6MzIsImlkQ2l0eSI6NDkwfQ.bJa1eSG1fw9vBbYBf2ZqwNlLPFffUjg6b8N2n9zlzGU',
+        'authorization': 'Bearer ' + user_token,
         'origin': 'https://journal.top-academy.ru',
         'priority': 'u=1, i',
         'referer': 'https://journal.top-academy.ru/',
