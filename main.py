86ac7a110590a0a3f57f11c9c72e1988ab31d917
soniiaaii <sonosokolov2513@gmail.com>
2024-12-12 11:34:41 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,56 @@
 import logging
 import asyncio
-from sched import scheduler
-
-from aiogram import Dispatcher, Bot, types
+import time
+import datetime
+from aiogram.types import UNSET_PARSE_MODE
+from datetime import date, timedelta
+from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command, Filter
 from aiogram.types import FSInputFile
+from apscheduler.schedulers.blocking import BlockingScheduler
+from pyexpat.errors import messages
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from pytz import utc
+
 
 logging.basicConfig(level=logging.INFO)
-TOKEN = '8122833408:AAFdg78LuB8AJFWUFaeU4pB8bMJB_uBM3Lo'
+TOKEN = "7694855874:AAGptMKWQ1OsSN_sncjbtPKrPHZs-Byczio"
+
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
-# привет это Софи
 
-@dp.message(Command("start"))
-async def start(message: types.Message):
-    await message.reply('Hello world!' + message.from_user.username, parse_mode='pre-formatted fixed-width code block')
+duty = ["Хаял", "Влад", "Данила", "Кирилл", "Арслан", "Ярослав", "Артем Карпенко", "Наиль", "Илья Парфенов", "Роза",
+        "Егор", "Ралим", "Соня", "Марат", "Илья Маслов", "Артем Ченцов", "Фаезбек", "Артур Биккузин"]
 
-
-@dp.message()
-async def echo(message: types.Message):
-    a = FSInputFile("1.jpg")
-    await message.answer_photo(a)
-
-# Илья Маслов
-async def wake_up_members():
-    await bot.send_message(chat_id='-1002312275639', text="Время вставать!")
+current_date = date.today()
+counter = 0
 
 
-scheduler.add_job(wake_up_members, 'cron', hour=13, minute=39)
+@dp.message(Command("startSophie")) #начальная команда
+async def check_and_send_duty(message: types.Message): #наша функция
+    global counter
+    today = datetime.date.today()
+    if today.weekday() in [5,6]: #если выходные то пропускаем
+        pass
+    else:
+        if today.weekday() in [0,1,2,3,4]: #если рабочие дни
+            if counter >= len(duty): #как список с дежурными заканчивается,сбрасываем его до нуля
+                counter = 0
+            name = duty[counter]
+            counter = (counter + 1)  # счетчик дежурного
+            await message.answer(f'Сегодня дежурный: {name}')
 
 
-async def start_dp():
+        scheduler = AsyncIOScheduler()
+        scheduler.configure(timezone=utc)
+        scheduler.add_job(check_and_send_duty, 'cron', hour=13, minute=00,args=[message])
+
+
+
+async def start_db():
     await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
-    asyncio.run(start_dp())
+    asyncio.run(start_db())
